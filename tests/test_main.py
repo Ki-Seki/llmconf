dbd687cb8aa9eab4223ac5e92e46de7bf5c6f926
Shichao Song <song.shichao@outlook.com>
2024-10-29 17:41:46 +0800

    feat (transformers): done

--- tests/test_main.py
+++ tests/test_main.py
@@ -41,6 +41,25 @@ class TestTransformers(unittest.TestCase):
             "TransformersConf {'pretrained_model_name_or_path': 'gpt-3.5-turbo', 'max_new_tokens': 100}",
         )
 
+    def test_to_transformers2(self):
+        conf1 = LLMConf(model="tmp", max_completion_tokens=24, max_new_tokens=8)
+        conf2 = conf1.transformers
+        self.assertEqual(
+            str(conf2),
+            "TransformersConf {'pretrained_model_name_or_path': 'tmp', 'max_new_tokens': 8}",
+        )
+
+    def test_to_specific_cases(self):
+        conf = LLMConf(model="model_a", max_tokens=100, query="Hi")
+        self.assertEqual(
+            str(conf.transformers.from_pretrained),
+            "{'pretrained_model_name_or_path': 'model_a'}",
+        )
+        self.assertEqual(
+            str(conf.transformers.generation_config),
+            "{'max_new_tokens': 100}",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
